ece77a076f65d281857f8ac01946b6c7f774b913
DylanJimenezQ <dylanjimenez912@gmail.com>
2024-07-29 20:17:46 -0600

    paginas de productos con filtro lista

--- js/listclothes.js
+++ js/listclothes.js
@@ -1,13 +1,13 @@
 function DetalleProducto(id) {
-    window.location.href = `detalle-ropa.html?id=${id}`;
+  window.location.href = `detalle-ropa.html?id=${id}`;
 }
 function displayclothesmoresold(data) {
-    $("#clothe-list").html('')
-    data.forEach((clothes) => {
-        if (clothes.categories[0] == "Mas vendidos") {
-            const colclothes = document.createElement("div")
-            colclothes.classList.add("col")
-            const cardclothes = `<div class="col">
+  $("#clothe-list").html('')
+  data.forEach((clothes) => {
+    if (clothes.categories[0] == "Mas vendidos") {
+      const colclothes = document.createElement("div")
+      colclothes.classList.add("col")
+      const cardclothes = `<div class="col">
             <div class="card">
               <img class="card-img-top" style="max-height:260px;" role="img" src="${clothes.imgprincipalUrl ? clothes.imgprincipalUrl : 'img/image-not-found.jpg'}"
                 alt="Imagen" />
@@ -20,22 +20,20 @@ function displayclothesmoresold(data) {
                 <hr>
                 <h1 class="card-title text-end">&cent;${clothes.price}</h1>
                 <div class="d-grid gap-2">
-                  <button type="button" class="btn btn-lg " onclick="detalleLibro(${clothes._id})">VER</button>
+                  <button type="button" class="btn btn-outline-primary " onclick="detalleLibro(${clothes._id})">VER</button>
                 </div>
               </div>
             </div>
           </div>`
-            $("#clothe-list").append(cardclothes)
-        }
-    });
+      $("#clothe-list").append(cardclothes)
+    }
+  });
 }
 function displayclothespromotion(data) {
-    $("#clothe-list-promo").html('')
-    data.forEach((clothes) => {
-        if (clothes.categories[0] == "Promocion") {
-            const colclothes = document.createElement("div")
-            colclothes.classList.add("col")
-            const cardclothes = `<div class="col">
+  $("#clothe-list-promo").html('')
+  data.forEach((clothes) => {
+    if (clothes.categories[0] == "Promocion") {
+      const cardclothes = `<div class="col">
             <div class="card">
               <img class="card-img-top" style="max-height:260px;" role="img" src="${clothes.imgprincipalUrl ? clothes.imgprincipalUrl : 'img/image-not-found.jpg'}"
                 alt="Imagen" />
@@ -44,29 +42,85 @@ function displayclothespromotion(data) {
                 <hr>
                 <p class="fs-4">${clothes.subdescription}</p>
                 <hr>
-                <p class="fs-5">${clothes.price}</p>
+                <p class="fs-5" style="text-decoration:line-through">Precio Regular: &cent${clothes.price}</p>
                 <hr>
-                <h1 class="card-title text-end">2x&cent;${clothes.pricepromo}</h1>
+                <h1 class="card-title text-end" style="font-weight:bold">2x&cent;${clothes.pricepromo}</h1>
                 <div class="d-grid gap-2">
-                  <button type="button" class="btn btn-lg " onclick="detalleLibro(${clothes._id})">VER</button>
+                  <button type="button" class="btn btn1 btn-outline-primary" onclick="detalleLibro(${clothes._id})">VER</button>
                 </div>
               </div>
             </div>
           </div>`
-            $("#clothe-list-promo").append(cardclothes)
-        }
+      $("#clothe-list-promo").append(cardclothes)
     }
-    )
+  }
+  )
 }
+function displayclothes(data) {
+  $("#productos").html('')
+  data.forEach((clothes) => {
+    const colclothes = document.createElement("div")
+    colclothes.classList.add("col")
+    let pricePromoHtml = ''
+    if (clothes.pricepromo) {
+      pricePromoHtml = `<h1 class="card-title text-end"style="font-weight:bold">2x&cent;${clothes.pricepromo}</h1><hr>`
+    }
+    const cardclothes = `<div class="col">
+            <div class="card">
+              <img class="card-img-top" style="max-height:260px;" role="img" src="${clothes.imgprincipalUrl ? clothes.imgprincipalUrl : 'img/image-not-found.jpg'}"
+                alt="Imagen" />
+              <div class="card-body">
+                <p class="fs-3 card-title">${clothes.name}</p>
+                <hr>
+                <p class="fs-4">${clothes.subdescription}</p>
+                <hr>
+                <p class="fs-5">${clothes.talla}</p>
+                <hr>
+                <h1 class="card-title text-end">&cent;${clothes.price}</h1>
+                <hr>
+                 ${pricePromoHtml}
+                <div class="d-grid gap-2">
+                  <button type="button" class="btn btn-outline-primary " onclick="detalleLibro(${clothes._id})">VER</button>
+                </div>
+              </div>
+            </div>
+          </div>`
+    $("#productos").append(cardclothes)
 
-
+  });
+}
+function displaycategories(){
+  var select = $('#filter');
+  var categories = [];
+  $.each(clothes,function(index,clothe){
+    $.each(clothe.categories, function(index,category){
+      if($.inArray(category,categories)=== -1){
+        categories.push(category)
+        //Añadir opción al select
+        select.append(`<option value="${category}"> ${category} </option>`)
+      }
+    })
+  })
+}
+  
 
 $(document).ready(function () {
-    
 
-    displayclothesmoresold(clothes)
-    displayclothespromotion(clothes)
-    
-    $('#filter').change(function () {
-    });
+
+  displayclothesmoresold(clothes)
+  displayclothespromotion(clothes)
+ displayclothes(clothes)
+ displaycategories()
+  $('#filter').change(function () {
+    var category=$(this).val()
+    var filterclothes
+    if(category==='all'){
+      filterclothes=clothes
+    }else{
+      filterclothes=clothes.filter((function(clothe){
+        return clothe.categories.includes(category)
+      }))
+    }
+    displayclothes(filterclothes)
+  });
 });
