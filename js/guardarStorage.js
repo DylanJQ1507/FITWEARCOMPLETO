8fe28e31c9d8bf505fe247133796a0a5b920c976
BrandonSG2 <brandonsoto3006@gmail.com>
2024-08-19 18:46:39 -0600

    agregado validacion para promos y se aregla galeria imagenes

--- js/guardarStorage.js
+++ js/guardarStorage.js
@@ -8,19 +8,19 @@ $(document).ready(function () {
     // Obtén el valor del parámetro 'id'
     var id = urlObj.searchParams.get('id');
 
-    const clothe = clothes.find((b) => b._id == id)
-    console.log(id)
+    const clothe = clothes.find((b) => b._id == id);
+    console.log(id);
+    
     const comprarProducto = document.getElementById("compraa");
     comprarProducto.addEventListener("click", (e) => {
         e.preventDefault();
-        var pricepromo;
+        
+        // Obtener detalles del producto
         const idproducto = clothe._id;
         const name = clothe.name;
         const img = clothe.imgprincipalUrl;
         const price = clothe.price;
-        if (clothe.pricepromo) {
-            pricepromo = clothe.pricepromo;
-        }
+        const pricepromo = clothe.pricepromo || price; // Usa el precio promocional si está disponible
         const cantidad = document.getElementById("Cantidad").value;
         const tamaño = document.getElementById("tallas").value;
         const cantf = parseInt(cantidad, 10);
@@ -30,33 +30,32 @@ $(document).ready(function () {
             idproducto,
             name,
             img,
-            price,
+            price: pricepromo, 
             cantf,
             tamaño,
-            pricepromo,
+            pricepromo, 
         };
 
-        // Guardar o actualizar producto en localStorage
+        
         saveclotheLocalStorage(producto);
         $.notify("Producto Agregado con Exito: " + name, "success");
     });
 
     const saveclotheLocalStorage = (producto) => {
         let productos = JSON.parse(localStorage.getItem("productos")) || [];
-        // Buscar si el producto ya existe
+        
         const index = productos.findIndex(p => p.idproducto === producto.idproducto);
         
         if (index > -1) {
-           
             productos[index].cantf += producto.cantf;
-           
+            
             productos[index].total = (productos[index].pricepromo || productos[index].price) * productos[index].cantf;
         } else {
             
+            producto.total = (producto.pricepromo || producto.price) * producto.cantf;
             productos.push(producto);
         }
         
         localStorage.setItem("productos", JSON.stringify(productos));
     };
-
 });
